98b88079603dec9b5f36055d34c3e295560398a9
Fateme Arjomandi <farjomandy76@gmail.com>
2023-02-12 18:59:30 +0330

    initoial homePageContoroller and home page

---
+++ lib/home_page_controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+class HomePageController extends GetxController {
+
+  
+  final count = 0.obs;
+  increment() => count.value++;
+
+  final HomePageController controller = Get.put(HomePageController());
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       
-      
+      //home: const Splash(),
     );
     }}
