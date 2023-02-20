15da60fed4bc1e5cbcb302a56918cbfa27c05f7b
Fateme Arjomandi <farjomandy76@gmail.com>
2023-02-20 12:00:37 +0330

    initiaol obx getx

--- lib/pages/second.viwe.dart
+++ lib/pages/second.viwe.dart
@@ -4,23 +4,31 @@ import 'package:get/get.dart';
 import '../home_page_controller.dart';
 
 class Second extends StatelessWidget {
-Second({super.key});
+  Second({super.key});
   final homePageController = Get.put(HomePageController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:const Text('Second page')
-      ),
+      appBar: AppBar(title: const Text('Second page')),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('Number is : 0', style: Theme.of(context).textTheme.bodyText1,),
-            TextButton(onPressed: () {}, child: Text('Actoin', style: Theme.of(context).textTheme.bodyText1,))
-          ]),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Obx(
+                () =>  Text(
+                  'Number is : ${homePageController.count.value}',
+                  style: Theme.of(context).textTheme.bodyText1,
+                ),
+              ),
+              TextButton(
+                  onPressed: () => homePageController.increment(),
+                  child: Text(
+                    'Actoin',
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ))
+            ]),
       ),
     );
   }
