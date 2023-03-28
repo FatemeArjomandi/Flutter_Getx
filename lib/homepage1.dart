188f878489146008a3ba8849c2bc90cdefb67b36
Fateme Arjomandi <farjomandy76@gmail.com>
2023-03-28 12:15:41 +0330

    remove class MyHomePage and initial class HomePage1

---
+++ lib/homepage1.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class HomePage1 extends StatelessWidget {
+  const HomePage1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
