1e6fba61b2a8e5603ebb1331a52479d12e901c03
Fateme Arjomandi <farjomandy76@gmail.com>
2023-03-28 12:21:13 +0330

    initial homepage2

---
+++ lib/homepage2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomePage2 extends StatelessWidget {
+  const HomePage2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
