0da68738567bfa2b246524cbacb208f63eca2691
Fateme Arjomandi <farjomandy76@gmail.com>
2023-03-31 02:27:42 +0330

    initial scrin1 and scrin2

---
+++ lib/scrin1.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/scrin2.dart';
+import 'package:get/get.dart';
+
+class Scrin1 extends StatelessWidget {
+  const Scrin1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Get.to(()=>const Scrin2());
+        },
+        child: const Text(
+          'Scrin1',
+          style: TextStyle(fontSize: 30, color: Colors.green),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/scrin2.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class Scrin2 extends StatelessWidget {
+  const Scrin2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          Get.back();
+        },
+        child:const Center(
+          child:  Text(
+            'Scrin2',
+            style: TextStyle(fontSize: 30, color: Colors.green),
+          ),
+        ),
+      ),
+    );
+  }
+}
