44f6570f2141d9656905652044c192bf206304a1
Fateme Arjomandi <farjomandy76@gmail.com>
2023-04-01 03:23:38 +0330

    add ElevatedButton.icon

--- lib/bottomsheet.dart
+++ lib/bottomsheet.dart
@@ -8,6 +8,7 @@ class Scrin3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange,
         onPressed: () {
           Get.bottomSheet(Container(
             padding: const EdgeInsets.symmetric(horizontal: 30.0),
@@ -19,32 +20,50 @@ class Scrin3 extends StatelessWidget {
                     topLeft: Radius.circular(18))),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              children:  [
+              children: [
                 const SizedBox(
                   height: 30,
                 ),
                 const Text(
                   'Bottom sheet',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold
-                  ),
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
                 ),
                 const SizedBox(
                   height: 20,
                 ),
                 const Text(
-                  'gfvyagygyglYGY',
-                  style: TextStyle(fontSize: 18),
+                  'ka haal chal sabb badiya',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
                 ),
                 const SizedBox(
-                  height: 20,
+                  height: 50,
                 ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    ElevatedButton.icon(style:ElevatedButton.styleFrom(primary:Colors.red),onPressed: (){Get.back();}, icon:const Icon(Icons.thumb_down,), label:const Text('No')),
-                    const SizedBox(width: 30,),
-                    ElevatedButton.icon(onPressed: (){Get.back();}, icon:const Icon(Icons.thumb_up, color: Colors.blue,), label:const Text('yes'))
+                    ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red),
+                        onPressed: () {
+                          Get.back();
+                        },
+                        icon: const Icon(
+                          Icons.thumb_down,
+                        ),
+                        label: const Text('No')),
+                    const SizedBox(
+                      width: 30,
+                    ),
+                    ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green),
+                        onPressed: () {
+                          Get.back();
+                        },
+                        icon: const Icon(
+                          Icons.thumb_up,
+                        ),
+                        label: const Text('Yes'))
                   ],
                 )
               ],
@@ -55,10 +74,7 @@ class Scrin3 extends StatelessWidget {
       ),
       body: const Center(
           child: Text('Buttom Sheet ?',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.orange,
-                  fontWeight: FontWeight.bold))),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600))),
     );
   }
 }
