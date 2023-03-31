81f3dfe35a7f00eacfce62f04597b05213e7f225
Fateme Arjomandi <farjomandy76@gmail.com>
2023-03-31 12:20:11 +0330

    add floatingActionButton and bottomsheet

--- lib/bottomsheet.dart
+++ lib/bottomsheet.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Scrin3 extends StatelessWidget {
   const Scrin3({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      //floatingActionButtonAnimator: ,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Get.bottomSheet(
+           Container(
+            color: Colors.blue,
+            padding: const EdgeInsets.symmetric(horizontal: 30.0),
+           )
+          );
+        },
+        child: const Icon(Icons.double_arrow),
+      ),
+      body: const Center(
+          child: Text('Buttom Sheet ?',
+              style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.orange,
+                  fontWeight: FontWeight.bold))),
+    );
   }
 }
