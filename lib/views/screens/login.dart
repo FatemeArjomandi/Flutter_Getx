56c42d8ce41d037d5042af08c1a71aa99122707b
Fateme Arjomandi <farjomandy76@gmail.com>
2023-04-16 15:00:38 +0330

    add image assets

--- lib/views/screens/login.dart
+++ lib/views/screens/login.dart
@@ -6,19 +6,28 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:const Color(0xff154c79),
+      backgroundColor: const Color(0xff154c79),
       body: SingleChildScrollView(
         child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Column(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  
-                )
-
-              ],
-            ),),
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
+              Container(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'assets/images/parking.png',
+                  fit: BoxFit.cover,
+                  width: 180,
+                  height: 190,
+                  color: Colors.white70,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
