8f96fa8ec13606bd0d237514017e8515c45da25e
Fateme Arjomandi <farjomandy76@gmail.com>
2023-04-17 17:25:16 +0330

    add TextFromFields Register

--- lib/views/screens/register.dart
+++ lib/views/screens/register.dart
@@ -8,16 +8,77 @@ class RegisterScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xff154c79),
       body: SafeArea(
-          child: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-        child: Column(
-          children: [
-          Container(
-            margin: const EdgeInsets.only(top: 30),
-          )
-          ],
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            children: [
+              Container(
+                margin: const EdgeInsets.only(top: 10),
+                alignment: Alignment.center,
+                child: const Text(
+                  'Register New User',
+                  style: TextStyle(fontSize: 25, color: Colors.white),
+                ),
+              ),
+              const SizedBox(
+                height: 35,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your username'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your name'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Enter your email'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Enter your acont'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Enter your password'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Confirm password'),
+              ),
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
